678757ef1e92596c32e1db96bb2056a3b8e90e49
coraliecoumes <61029966+coraliecoumes@users.noreply.github.com>
2020-09-10 14:59:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
-<?php include_once("core-map-en.HTML"); 
-<a href="eco-sante-en.HTML">ecosante</a>
-<a href="wildlife-en.HTML">wildlife</a>
-<a href="eco-parcour-en.HTML">parcours</a> ?>
+<?php include_once("core-map-en.HTML"); ?>
